d981a8cfa3dcee30ed9d7ce1f77be688aaed6b40
[NilaBeast] <[manika.basak1977@gmail.com]>
2025-12-31 22:53:51 +0530

    initial commit

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173", // Vite frontend
-      "http://localhost:5000", // CRA frontend
+      "https://mini-world-of-alice.vercel.app", // CRA frontend
     ],
     credentials: true,
   })
